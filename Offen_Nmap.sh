65a571859c73ed85bd7023429a671f8c81222f7a
hackerassociate <62351511+hackerassociate@users.noreply.github.com>
2021-06-28 21:17:58 +0530

    Update Offen_Nmap.sh

---
+++ Offen_Nmap.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+echo -e "\n
+-----------------------------------------------------------------------------------------------------
+			    Welcome to Offensive IOT by Harshad!
+                            
+                Harsh BOT Ready for scanning. 
+                            
+                Provide Infra/Embedded System IP  for Scanning.
+
+			    #Author = Harshad Shah
+-----------------------------------------------------------------------------------------------------"
+
+echo -e "\nWelcome to Offensive IOT Scanning Program!\n"
+
+read -p "Enter the IP or WEB(eg. 11.12.12.45) : " IP_ADDR
+
+nmap -v -sCTV -Pn -T4 -p- -oA nmap_all_tcp  $IP_ADDR
+
+echo -e  "\nThank You Scanning done!\n"
